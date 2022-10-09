b44ead631f825526cc189f5368899a72f845b6b4
patkamon <patkamon.a@ku.th>
2022-10-09 22:40:31 +0700

    showing character

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react";
 import { getAuth, onAuthStateChanged, signInAnonymously } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, onDisconnect } from "firebase/database";
+import {
+  getDatabase,
+  ref,
+  set,
+  onDisconnect,
+  onValue,
+  onChildAdded,
+} from "firebase/database";
 import { createName } from "../utils/createName";
 import { randomFromArray } from "../utils/randomFromArray";
 import { getRandomSafeSpot } from "../utils/getRandomSafeSpot";
@@ -21,16 +28,18 @@ export default function Home() {
   const database = getDatabase();
 
   const [playerNameInput, setPlayerNameInput] = useState("");
-  // const gameContainer = document.querySelector(".game-container");
+  const [allPlayers, setAllPlayers] = useState([]);
+  // const [allPlayersRef, setAllPlayersRef] = useState(undefined);
+  const [allCoins, setAllCoins] = useState([]);
+  const [load, setLoad] = useState(true);
+
   // const playerColorButton = document.querySelector("#player-color");
 
   const playerColors = ["blue", "red", "orange", "yellow", "green", "purple"];
 
   useEffect(() => {
     signInAnonymously(auth)
-      .then(() => {
-        console.log("login");
-      })
+      .then(() => {})
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
@@ -38,7 +47,6 @@ export default function Home() {
       });
 
     onAuthStateChanged(auth, (user) => {
-      console.log(user);
       if (user) {
         //You're logged in!
         playerId = user.uid;
@@ -63,12 +71,27 @@ export default function Home() {
         onDisconnect(playerRef).remove();
 
         //Begin the game now that we are signed in
-        initGame();
+        let allPlayersRef = ref(database, `players`);
+        const allCoinsRef = ref(database, `coins`);
+        setLoad(false);
+
+        onChildAdded(allPlayersRef, (snapshot) => {
+          setAllPlayers((allPlayers) => [...allPlayers, snapshot.val()]);
+        });
+        console.log(allPlayers, "hello");
       } else {
         //You're logged out.
       }
     });
   }, []);
+
+  useEffect(() => {
+    console.log(allPlayers);
+  }, [allPlayers]);
+
+  // if (allPlayersRef) {
+
+  // }
 
   function handleArrowPress(xChange = 0, yChange = 0) {
     const newX = players[playerId].x + xChange;
@@ -89,13 +112,56 @@ export default function Home() {
   }
 
   function initGame() {
-    const allPlayersRef = ref(database, `players`);
-    const allCoinsRef = ref(database, `coins`);
+    // console.log(allCoinsRef, allPlayersRef);
+    // setLoad(false);
+    // onValue(allPlayersRef, (snapshot) => {
+    //   console.log(snapshot.val());
+    //   players = snapshot.val() || {};
+    //   Object.keys(players).forEach((key) => {
+    //     const characterState = players[key];
+    //     let el = playerElements[key];
+    //     // Now update the DOM
+    //     el.querySelector(".Character_name").innerText = characterState.name;
+    //     el.querySelector(".Character_coins").innerText = characterState.coins;
+    //     el.setAttribute("data-color", characterState.color);
+    //     el.setAttribute("data-direction", characterState.direction);
+    //     const left = 16 * characterState.x + "px";
+    //     const top = 16 * characterState.y - 4 + "px";
+    //     el.style.transform = `translate3d(${left}, ${top}, 0)`;
+    //   });
+    // });
   }
 
   return (
     <div>
-      <div className="game-container"></div>
+      <div className="game-container">
+        {!load &&
+          allPlayers.map((e, i) => {
+            // console.log(e);
+            const left = 16 * e.x + "px";
+            const top = 16 * e.y - 4 + "px";
+            const styles = {
+              transform: `translate3d(${left}, ${top}, 0)`,
+            };
+            return (
+              <div
+                key={i}
+                className={`Character grid-cell ${
+                  e.id === playerId ? "you" : ""
+                } `}
+                style={styles}
+              >
+                <div className="Character_shadow grid-cell"></div>
+                <div className="Character_sprite grid-cell"></div>
+                <div className="Character_name-container">
+                  <span className="Character_name">{e.name}</span>
+                  <span className="Character_coins">{e.coins}</span>
+                </div>
+                <div className="Character_you-arrow"></div>
+              </div>
+            );
+          })}
+      </div>
       <div className="player-info">
         <div>
           <label htmlFor="player-name">Your Name</label>
